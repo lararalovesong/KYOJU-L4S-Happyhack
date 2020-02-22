b11f2c3268fb16897977bf29ca90674fe6c97ce5
LaraShimada <laralachan@LaraShimadanoMacBook-Pro.local>
2020-02-22 22:45:13 +0900

    変更し た内容をメモ

--- fadein.js
+++ fadein.js
@@ -1,13 +1,13 @@
-
-    $(function(){
-        var setImg = '#viewer';
-        var fadeSpeed = 1000;
-        var switchDelay = 3000;
-      
-        $(setImg).children('img').css({opacity:'0'});
-        $(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'-1'},fadeSpeed);
-      
-        setInterval(function(){
-            $(setImg + ' :first-child').animate({opacity:'0',zIndex:'-2'},fadeSpeed).next('img').animate({opacity:'1',zIndex:'-1'},fadeSpeed).end().appendTo(setImg);
-        },switchDelay);
+$(function(){
+　$(window).scroll(function (){
+    $('.effect-fade').each(function(){
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight+200){
+            $(this).addClass('effect-scroll');
+        }
     });
+　});
+});
+
